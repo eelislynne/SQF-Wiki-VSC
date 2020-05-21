a8d25bb18e21091dfeca9eb26f9df31a72d49b79
Fini21 <eelis.lynne@gmail.com>
2020-05-21 17:31:53 +0300

    "improvements" btw

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# sqf-wiki README
+# sqf-wiki
 
 SQF-Wiki is an extension that allows users to easily look up commands on the BI wiki.
 
 ## Features
 
 Just highlight the command you'd like to lookup and right click or press the hotkey ctrl+shift+q
+Updated 2020, you can now just right click on a function. No need to highlight the text anymore!
 
 ## Works with SQF Language
+
 
 **Have fun!**
--- extension.js
+++ extension.js
@@ -1,46 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const editor = vscode.window.activeTextEditor;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "sqf-wiki" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.showWiki', function () {
-        // The code you place here will be executed every time your command is executed
-
-        HandleCall();
-    });
-
-    context.subscriptions.push(disposable);
-}
-exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
-
+exports.activate = (ctx) => {
+    const disposable = vscode.commands.registerCommand('extension.showWiki', HandleCall);
+    ctx.subscriptions.push(disposable);
+};
 
 function HandleCall() {
-    var func = "";
-    if (!editor.selection.isEmpty) {
-        var text = editor.document.getText(editor.selection);
+    const text = getWord();
 
-        var parts = text.split(" ");
-        func = parts[0];
-
-        var url = "https://community.bistudio.com/wiki/" + func;
-        vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(url));
-    } else {
-        vscode.window.showWarningMessage("Selection is empty");
+    if (!text || text.length > 64) {
+        vscode.window.showWarningMessage('Selection is empty');
+        return;
     }
+
+    const url = `https://community.bistudio.com/wiki/${text}`;
+    vscode.env.openExternal(vscode.Uri.parse(url));
+}
+
+function getWord() {
+
+    const editor = vscode.window.activeTextEditor;
+    const sel = editor.selection;
+
+    if (!sel.isEmpty) {
+        return editor.document.getText(sel);
+    }
+
+    const cursor = sel.start;
+    const range = editor.document.getWordRangeAtPosition(cursor);
+    const currentWord = editor.document.getText(range);
+
+    return currentWord;
 }
